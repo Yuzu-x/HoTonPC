4a556f5f80ac8b2c902a45946d5f3e5de31fe98d
Yuzu-x <54381207+Yuzu-x@users.noreply.github.com>
2020-03-10 12:32:22 +1100

    Added on screen joystick controller

---
+++ PhysicsGame/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public Rigidbody player;
+    public GameObject mouseTracker;
+
+    public float moveSpeed = 6f;
+    public float playerHeading;
+    public float rotationSpeed = 6f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+
+        var forward = Camera.main.transform.forward;
+        var right = Camera.main.transform.right;
+
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+
+        var moveDirection = forward * verticalAxis + right * horizontalAxis;
+
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+
+        player.rotation = Quaternion.Euler(0, playerHeading, 0);
+
+        if(Input.GetKey(KeyCode.Q))
+        {
+            playerHeading = playerHeading - rotationSpeed;
+        }
+
+        if(Input.GetKey(KeyCode.E))
+        {
+            playerHeading = playerHeading + rotationSpeed;
+        }
+
+    }
+}
---
+++ PhysicsGame/Assets/Scripts/RaycastFromcam.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastFromcam : PlayerControls
+{
+    float selectionDistance;
+    Vector3 screenTargetPosition;
+    Vector3 rigidBodyPosition;
+
+    Rigidbody toy;
+    public float force = 300f;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            toy = CheckMouse();
+        }
+
+        if(Input.GetMouseButtonDown(0) && toy)
+        {
+            toy = null;
+        }
+ 
+   }
+
+    public void FixedUpdate()
+    {
+        if(toy)
+        {
+            Vector3 mousePositionOffset = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, selectionDistance)) - screenTargetPosition;
+            toy.velocity = (rigidBodyPosition + mousePositionOffset - toy.transform.position) * force * Time.deltaTime;
+        }
+
+        CheckMouse();
+    }
+
+    Rigidbody CheckMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit selectable;
+
+        if (Physics.Raycast(ray, out selectable))
+        {
+            if (selectable.collider.gameObject.GetComponent<Rigidbody>())
+            {
+                selectionDistance = Vector3.Distance(ray.origin, selectable.point);
+                screenTargetPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, selectionDistance));
+                rigidBodyPosition = selectable.collider.transform.position;
+                return selectable.collider.gameObject.GetComponent<Rigidbody>();
+
+            }
+            Debug.Log(selectable);
+        }
+        return null;
+    }
+}
